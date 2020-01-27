c935072f955030b607804ebec9f1e6d6fbff90dc
Thomas Boerger <thomas@webhippie.de>
2020-01-27 09:55:18 +0100

    Remove single quotes with double quotes

--- Casks/imusic.rb
+++ Casks/imusic.rb
@@ -1,10 +1,10 @@
-cask 'imusic' do
-  version '2.0.8'
-  sha256 '0a67bfda8d42712ecd4a2497577aac708443e67559da7c51d924a56c1ea83c12'
+cask "imusic" do
+  version "2.0.8"
+  sha256 "0a67bfda8d42712ecd4a2497577aac708443e67559da7c51d924a56c1ea83c12"
 
   url "http://download.aimersoft.com/cbs_down/imusic-mac_full2401.dmg"
-  name 'iMusic'
-  homepage 'https://imusic.aimersoft.com'
+  name "iMusic"
+  homepage "https://imusic.aimersoft.com"
 
-  app 'iMusic.app'
+  app "iMusic.app"
 end
